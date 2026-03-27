f6fa4145e0fd2e91fb8cfd5efc1a2a5b061a4fff
se207forar-del <se207forar@gmail.com>
2026-03-27 11:25:21 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 ﻿window.APP_CONFIG = {
   // 部署 GAS 後貼上 Web App URL，例如：https://script.google.com/macros/s/xxxxx/exec
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbwXZ_yNPE0lXIF-WU9wECMu28VoN69aSwr7CoRuQN8ZyQ-DxSAy5K3lkqQQm48v5sbv6Q/exec"
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbzvOaaWcEs8gUGTS0s8zY_hvjT_fkrGfG1T8LynvpIU6GbpVXzLwW4iy8IUrhI_O5R3XQ/exec"
 };
